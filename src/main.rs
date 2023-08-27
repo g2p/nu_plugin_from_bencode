7c09d27eac03e8a3202c046e7badc9b510828911
Bryant Luk <code@bryantluk.com>
2023-08-27 13:15:44 -0500

    Fix clippy lints

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use nu_plugin::JsonSerializer;
 use nu_plugin_from_bencode::FromBencode;
 
 fn main() {
-    nu_plugin::serve_plugin(&mut FromBencode::default(), JsonSerializer {});
+    nu_plugin::serve_plugin(&mut FromBencode, JsonSerializer {});
 }
